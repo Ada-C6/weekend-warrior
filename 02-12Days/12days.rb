7741a80714ccdc9f40762ff27d1d403ab466b7cf
jm-rives <joanna.rives@gmail.com>
2016-08-28 20:28:12 -0700

    ADD case statement

--- 02-12Days/12days.rb
+++ 02-12Days/12days.rb
@@ -3,12 +3,31 @@ class Xmas
   # cardinal numbering
   def self.gifts(day_of_xmas)
 
-    if day_of_xmas == 0
+    case day_of_xmas 
+    when  0
       return ["a partridge in a pear tree"]
-    elsif day_of_xmas == 4 
+    when 1
+      return ["two turtle doves", "and a partridge in a pear tree"]
+    when 2
+      return ["three french hends"]
+    when 3
+      return ["four calling birds"]
+    when 4
       return ["FIVE GOLDEN RINGS"]
-    else
-      return ["two turtle doves", "and a partridge in a pear tree"] 
+    when 5
+      return ["six geese-a-laying"]
+    when 6
+      return ["seven swans-a-swimming"]
+    when 7
+      return ["eight maids-a-milking"]
+    when 8
+      return ["nine ladies dancing"]
+    when 9
+      return ["ten lords-a-leaping"]
+    when 10
+      return ["eleven pipers piping"]
+    when 11
+      return ["twelve drummers drumming"]
     end
   end
 
