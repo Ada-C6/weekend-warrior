4eba2dcba9e02115dc36819da66dfc772297a365
jm-rives <joanna.rives@gmail.com>
2016-09-11 11:18:41 -0700

    ADD loop to test Hamming distance

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,7 +1,7 @@
 class Hamming
 
   def self.compute(strand_1, strand_2)
-
+    # preconditions
     if strand_1 + strand_2 == ""
       
       return 0 
@@ -17,6 +17,34 @@ class Hamming
       raise ArgumentError.new("Strand_2 is longer!")
 
     end
+
+    # strand_1 & strand_2 are of equal length and are not empty
+
+    if strand_1 == strand_2
+
+      return 0
+
+    end
+
+    hamming_count = 0
+
+    # this is where the exciting stuff happens
+
+    strand_1.size.times do |count|
+
+      if strand_1[count] == strand_2[count]
+
+        next
+
+      else
+
+        hamming_count += 1
+
+      end
+
+    end
+
+    return hamming_count
 
   end
 
